82c737af5fbf32c7109294b5db80615a31be85e9
Micah Mustaham <mciahmustaham@gmail.com>
2024-12-09 21:34:16 +0800

    seeder implemented to seed the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Conversation;
+use App\Models\Group;
+use App\Models\Message;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // for admin
+        User::factory()->create([
+            'name' => 'Micah Mustaham',
+            'email' => 'micahmustaham@gmail.com',
+            'password' => bcrypt('micahmustaham'),
+            'is_admin' => true,
+        ]);
 
+        // for regular user
+        User::factory()->create([
+            'name' => 'Yami Mustaham',
+            'email' => 'yamimustaham@gmail.com',
+            'password' => bcrypt('yamimustaham'),
+        ]);
+
+        User::factory(10)->create();
+
+        for ($i = 0; $i < 5; $i++) {
+            $group = Group::factory()->create([
+                'owner_id' => 1,
+            ]);
+
+            $users = User::inRandomOrder()->limit(rand(2, 5))->pluck('id');
+            $group->users()->attach(array_unique([1, ...$users]));
+        }
+
+        Message::factory(1000)->create();
+        $messages = Message::whereNull('group_id')->orderBy('created_at')->get();
+
+        $conversations = $messages->groupBy(function ($message) {
+            return collect([$message->sender_id, $message->receiver_id])
+                ->sort()
+                ->implode('_');
+        })->map(function ($groupMessages) {
+            return [
+                'user_id1' => $groupMessages->first()->sender_id,
+                'user_id2' => $groupMessages->first()->receiver_id,
+                'last_message_id' => $groupMessages->last()->id,
+                'created_at' => new Carbon(),
+                'updated_at' => new Carbon(),
+            ];
+        })->values();
+
+        Conversation::insertOrIgnore($conversations->toArray());
     }
 }
